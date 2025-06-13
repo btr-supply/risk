292779edbda27836221b301a7049a5819d14e547
pde-rent <paul.ed.de.renty@gmail.com>
2025-06-13 18:05:30 +0700

    [chore] Remove legacy Vite and React Router files

--- src/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as HomePage } from './HomePage';
-export { default as AllocationModel } from './AllocationModel';
-export { default as LiquidityModel } from './LiquidityModel';
-export { default as SlippageModel } from './SlippageModel';
